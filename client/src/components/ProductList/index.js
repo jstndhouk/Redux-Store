dd995e4a4b91520dc84d2a22e58976b3ff2639c1
Justin Houk <jstndhouk@gmail.com>
2022-02-07 18:10:14 -0500

    fixed bugs

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -6,12 +6,12 @@ import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
-
+import { useDispatch, useSelector } from 'react-redux';
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
-
-  const { currentCategory } = state;
-
+  
+const dispatch = useDispatch();
+const state = useSelector((state) => state);
+const { currentCategory } = state;
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,23 +1,10 @@
-import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers'
+import React from "react";
+import { Provider } from 'react-redux';
+import store from './store';
 
-const StoreContext = createContext();
-const { Provider } = StoreContext;
+export default function StoreProvider (props) {
 
-const StoreProvider = ({ value = [], ...props }) => {
-  const [state, dispatch] = useProductReducer({
-    products: [],
-    cart: [],
-    cartOpen: false,
-    categories: [],
-    currentCategory: '',
-  });
-
-  return <Provider value={[state, dispatch]} {...props} />;
+  return <Provider store={store} {...props} />;
 };
 
-const useStoreContext = () => {
-  return useContext(StoreContext);
-};
-
-export { StoreProvider, useStoreContext };
+export { StoreProvider };
